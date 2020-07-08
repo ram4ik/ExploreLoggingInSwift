eabd64f717a40601dfea5b944430e9efa7be2ab0
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-08 20:46:31 +0300

    init log

--- ExploreLoggingInSwift/ContentView.swift
+++ ExploreLoggingInSwift/ContentView.swift
@@ -5,11 +5,18 @@
 //  Created by Ramill Ibragimov on 08.07.2020.
 //
 
+import os
 import SwiftUI
+
+let logger = Logger(subsystem: "com.ri.log", category: "test")
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        Button(action: {
+            logger.log("This message logged")
+        }, label: {
+            Text("Log it")
+        })
     }
 }
 
