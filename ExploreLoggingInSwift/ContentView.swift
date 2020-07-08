34000d3e1a00547e9d810a800d67bba1d8f31111
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-08 20:37:32 +0300

    Initial Commit

---
+++ ExploreLoggingInSwift/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ExploreLoggingInSwift
+//
+//  Created by Ramill Ibragimov on 08.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExploreLoggingInSwift/ExploreLoggingInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreLoggingInSwiftApp.swift
+//  ExploreLoggingInSwift
+//
+//  Created by Ramill Ibragimov on 08.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct ExploreLoggingInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
